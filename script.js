4825284ff01b94432c3f0fc9dc913b0e5cb76f0f
Rakesh <chipparakesh01@gmail.com>
2024-08-24 20:07:36 +0530

    modified the the code

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+const element = document.getElementsByClassName("offer");
+element[0].classList.add("expanded");
+document.getElementsByTagName("input")[0].checked = true;
+
 function checkHandler(event, element) {
   event.stopPropagation();
 
